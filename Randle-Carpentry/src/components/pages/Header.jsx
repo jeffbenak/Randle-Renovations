7d0780ff9b65188303ac5ecc323c1c5d46b8a941
Jeff Benak <jeff.benak@yahoo.com>
2023-02-25 21:51:00 -0600

    Added the logo to the header, and gave it a fixed width and height, and also added a transition where the image width and height will get smaller when the user scrolls down on the page. Added a border radius to the image as well so it doesn't have any edges

--- Randle-Carpentry/src/components/pages/Header.jsx
+++ Randle-Carpentry/src/components/pages/Header.jsx
@@ -1,24 +1,45 @@
 import React from "react";
+import Logo from "../logo/logo.jpg";
 
 export default function Header() {
 
     window.onscroll = function() {
         scroll();
+        animate();
     };
 
     function scroll() {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            document.getElementById("header").style.top = "-20px";
+            document.getElementById("head").style.top = "-20px";
         }  else if (document.body.scrollTop === 0 || document.documentElement.scrollTop === 0) {
-            document.getElementById("header").style.top = "0";
-        }
-
+            document.getElementById("head").style.top = "0";
+        } 
     }
-      
 
+    function animate() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("logo").style.width = "120px"; document.getElementById("logo").style.height = "100px";
+            }  else if (document.body.scrollTop === 0 || document.documentElement.scrollTop === 0) {
+                document.getElementById("logo").style.width = "140px"; document.getElementById("logo").style.height = "120px";
+            } 
+        
+    }
+
+    // function logo() {
+    //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    //         document.querySelector(".logo").style.width = "120px";
+    //     } else if (document.body.scrollTop === 0 || document.documentElement.scrollTop === 0) {
+    //         document.querySelector(".logo").style.width = "150px";
+    //     }
+    // }
+
+      
     return (
         <>
         <div id="head">
+
+
+            <img id="logo" src={Logo}></img>
 
             {/* <a href="#home">
             Home
